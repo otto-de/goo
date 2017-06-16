cf64ffd4383c30d602f3edc950162e9e9adf7013
Tjark Smalla <tjark.smalla@otto.de>
2017-06-16 16:49:32 +0200

    ts| remove second unused dependency

--- project.clj
+++ project.clj
@@ -16,7 +16,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [com.stuartsierra/component "0.3.2"]
                  [metrics-clojure "2.8.0"]]
   :profiles {:dev {:plugins [[lein-release/lein-release "1.0.9"]]}}
   )
