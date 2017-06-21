7ce7f5b55f7fc60c5515be0eebbc32f0161cba94
Tjark Smalla <tjark.smalla@otto.de>
2017-06-21 13:41:58 +0200

    tm/ts| Fix graphite exporter

--- src/de/otto/tesla/goo/goo_graphite.clj
+++ src/de/otto/tesla/goo/goo_graphite.clj
@@ -12,12 +12,10 @@
   (start [self]
     (let [host (get-in config [:config :graphite-host])
           port (get-in config [:config :graphite-port])
-          exporter (GraphiteExporter. host port)
           interval-in-ms (* 1000 (get-in config [:config :graphite-interval-seconds]))
           r (.raw (goo/snapshot))]
       (log/info "Starting Goo Graphite")
-
-      (at/every interval-in-ms #(.push exporter r) (sched/pool scheduler) :desc "Goo Graphite"))
+      (at/every interval-in-ms #(GraphiteExporter/push host port r) (sched/pool scheduler) :desc "Goo Graphite"))
     )
   (stop [self]
     (log/info "Stopping Goo Graphite")
