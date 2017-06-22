193b6a5e2cf74a4689c465e6d995316e55f3fbbf
Tjark Smalla <tjark.smalla@otto.de>
2017-06-22 10:58:44 +0200

    tm/ts| Use hostname for grapite path; Hostname can be transformed by a custom function

--- src/de/otto/tesla/goo/goo_graphite.clj
+++ src/de/otto/tesla/goo/goo_graphite.clj
@@ -3,40 +3,45 @@
             [de.otto.tesla.stateful.scheduler :as sched]
             [de.otto.tesla.goo.goo :as goo]
             [overtone.at-at :as at]
-            [clojure.tools.logging :as log])
+            [clojure.tools.logging :as log]
+            [environ.core :as env]
+            [clojure.string :as cs])
   (:import (goo GraphiteExporter)))
 
+(defn hostname []
+  (or (:host env/env) (:host-name env/env) (:hostname env/env)
+      "localhost"))
 
-(defn sanitize-prefix [prefix]
-  (if (or (empty? prefix) (= \. (last prefix)))
-    prefix
-    (str prefix ".")))
+(defn build-prefix [prefix app-hostname hostname-transform]
+  (let [transformed (hostname-transform app-hostname)
+        p (cs/join "." (remove nil? [prefix transformed]))]
+    (str p (when-not (empty? p) "."))))
 
-(defn push-to-graphite [graphite-config]
+(defn push-to-graphite [graphite-config hostname-transform]
   (try
     (let [host (:host graphite-config)
           port (Integer/parseInt (:port graphite-config))
-          prefix (sanitize-prefix (:prefix graphite-config ""))
+          prefix (:prefix graphite-config)
+          sane-prefix (build-prefix  prefix (hostname) hostname-transform)
           r (.raw (goo/snapshot))]
-      (log/infof "Reporting to Graphite %s:%s with %s as prefix" host port prefix)
-      (GraphiteExporter/push host port prefix r))
+      (log/infof "Reporting to Graphite %s:%s with %s as prefix" host port sane-prefix)
+      (GraphiteExporter/push host port sane-prefix r))
     (catch Exception e
       (log/error e "Error while Reporting to Graphite"))))
 
-(defrecord GooGraphite [config scheduler]
+(defrecord GooGraphite [config scheduler hostname-transform]
   c/Lifecycle
   (start [self]
     (let [graphite-config (get-in config [:config :goo :graphite])
           interval-in-ms (* 1000 (:interval-in-s graphite-config))]
       (log/info "-> Starting Goo Graphite")
-      (at/every interval-in-ms #(push-to-graphite graphite-config) (sched/pool scheduler) :desc "Goo Graphite"))
+      (at/every interval-in-ms #(push-to-graphite graphite-config hostname-transform) (sched/pool scheduler) :desc "Goo Graphite"))
     self)
 
   (stop [self]
     (log/info "<- Stopping Goo Graphite")
     self))
 
-(defn new-goo-graphite []
-  (map->GooGraphite {}))
-
+(defn new-goo-graphite [conf]
+  (map->GooGraphite (merge {:hostname-transform identity} conf)))
 
---
+++ test/de/otto/tesla/goo/goo_graphite_test.clj
@@ -0,0 +1,17 @@
+(ns de.otto.tesla.goo.goo-graphite-test
+  (:require [clojure.test :refer :all]
+            [de.otto.tesla.goo.goo-graphite :as gg]))
+
+(deftest build-prefix-test
+  (is (= "prefix.hostname." (gg/build-prefix "prefix" "hostname" identity)))
+  (is (= "hostname." (gg/build-prefix nil "hostname" identity)))
+  (is (= "prefix." (gg/build-prefix "prefix" nil identity)))
+  (is (= "" (gg/build-prefix nil nil identity))))
+
+(deftest hostname-transform-test
+  (is (= "prefix.abc." (gg/build-prefix "prefix" "host.name" (fn [hn] "abc"))))
+  (is (= "prefix.host." (gg/build-prefix "prefix" "host.name" #(re-find #"[^.]*" %)))))
+
+(deftest default-hostname-transform-test
+  (is (= identity
+         (:hostname-transform (gg/new-goo-graphite {})))))
