dbcba7bea4463429ac9b2c590df589973e820d9d
Tjark Smalla <tjark.smalla@otto.de>
2017-06-22 12:03:35 +0200

    tm/ts| Add console reporter

---
+++ src/de/otto/tesla/goo/goo_console.clj
@@ -0,0 +1,15 @@
+(ns de.otto.tesla.goo.goo-console
+  (:require [clojure.tools.logging :as log]
+            [overtone.at-at :as at]
+            [de.otto.tesla.stateful.scheduler :as sched]
+            [de.otto.tesla.goo.goo :as goo]))
+
+(defn write-to-console [console-config]
+  (log/info "Goo Console Reporting:")
+  (log/info (goo/text-format)))
+
+(defn start! [config scheduler]
+  (let [console-config (get-in config [:config :goo :console])
+        interval-in-ms (* 1000 (:interval-in-s console-config))]
+    (log/info "Starting goo console exporter")
+    (at/every interval-in-ms #(write-to-console console-config ) (sched/pool scheduler) :desc "Goo Console")))
