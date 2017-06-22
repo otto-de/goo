78b968c7afd7b704494d01cf46d3411a7b4c1411
tesla-tools-pipeline <tesla@dv.otto.de>
2017-06-22 10:59:23 +0200

    lein-release plugin: bumped version from 0.0.11 to 0.0.12-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto.tesla/goo "0.0.11"
+(defproject de.otto.tesla/goo "0.0.12-SNAPSHOT"
   :repositories [["central" {:url "http://artifactory.lhotse.ov.otto.de/artifactory/maven-central-remote"}]
                  ["clojars" {:url "http://artifactory.lhotse.ov.otto.de/artifactory/clojars-releases-remote"}]
                  ["nexus-releases" {:url "http://artifactory.lhotse.ov.otto.de/artifactory/nexus-releases-remote"}]
