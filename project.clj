d946a24bdbaf5a6f750439112335cdbdf89666f6
Tjark Smalla <tjark.smalla@otto.de>
2017-07-26 10:55:23 +0200

    lein-release plugin: preparing 0.2.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto.tesla/goo "0.2.0-SNAPSHOT"
+(defproject chillkroetetts/goo "0.2.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
