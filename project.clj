e6902aa80e4589078bcb8ec9440c6a9c79337183
Tjark Smalla <tjark.smalla@otto.de>
2017-07-26 12:29:28 +0200

    Next dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chillkroetetts/goo "0.2.0"
+(defproject chillkroetetts/goo "0.2.1-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
