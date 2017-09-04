21cc99e67360f6e3c4e297d65693923e81535c03
Carl Düvel <carl.duevel@otto.de>
2017-09-04 14:58:17 +0200

    lein-release plugin: bumped version from 1.1.0 to 1.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/goo "1.1.0"
+(defproject de.otto/goo "1.1.1-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/otto-de/goo"
