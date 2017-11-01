d11a646746f836ca1314597905ff333074cd172c
tsmalla <tjark.smalla@gmx.de>
2017-11-01 09:34:10 +0100

    lein-release plugin: preparing 1.2.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/goo "1.2.1-SNAPSHOT"
+(defproject de.otto/goo "1.2.1"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/otto-de/goo"
