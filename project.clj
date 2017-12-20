8b7d73defae86a93db57f7054a0f4748b5043351
Carl Düvel <carl.duevel@otto.de>
2017-12-20 21:23:34 +0100

    lein-release plugin: preparing 1.2.5 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/goo "1.2.5-SNAPSHOT"
+(defproject de.otto/goo "1.2.5"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/otto-de/goo"
